d7edff3c053972a3dbeb48c6e6cf8219e0c97bec
bunkercreativomx-maker <bunkercreativomx@gmail.com>
2026-09-15 21:17:26 +0000

    fix(bot): exportar engine en barrel + verificación e2e OK (slot_key UNIQUE bloquea doble reserva contra pb-creditos)

--- lib/creditos-bot/index.ts
+++ lib/creditos-bot/index.ts
@@ -2,6 +2,7 @@ export * from "./types";
 export * from "./policy";
 export * from "./time";
 export * from "./agenda";
+export * from "./engine";
 export * from "./openai-intent";
 export * from "./pocketbase";
 export * from "./turno";
